4776568615b36ea87190733c7076f765618164a0
Walter Mwaniki <mwaniki.faraja@gmail.com>
2024-02-18 18:42:56 -0500

    using npm instead of pnpm, and it worked

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), icon()]
+});
